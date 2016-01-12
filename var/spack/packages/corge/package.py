ab7b68631e96fa94ddfaa45951b6a68d7c31319b
James Amundson <amundson@fnal.gov>
2016-01-12 14:45:22 -0600

    corge package

---
+++ var/spack/packages/corge/package.py
@@ -0,0 +1,15 @@
+from spack import *
+
+class Corge(Package):
+    """Corge is a toy package"""
+    homepage = "https://github.com/amundson/corge"
+    url      = "http://compacc.fnal.gov/~amundson/corge-1.0.tar.gz"
+
+    version('1.0', 'c83ec110f677b67e0496260ccc7ceb81')
+
+    depends_on("quux")
+
+    def install(self, spec, prefix):
+        cmake(".", *std_cmake_args)
+        make()
+        make("install")
