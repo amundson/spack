d3ce6444eccf5536833288dc74d753519db59de7
James Amundson <amundson@fnal.gov>
2016-01-12 14:45:12 -0600

    quux package

---
+++ var/spack/packages/quux/package.py
@@ -0,0 +1,15 @@
+from spack import *
+
+class Quux(Package):
+    """Quux is a toy package"""
+    homepage = "https://github.com/amundson/quux"
+    url      = "http://compacc.fnal.gov/~amundson/quux-1.0.tar.gz"
+
+    version('1.0', 'e64c0dfbfdf43c17cd6aeeef79058734')
+
+    depends_on("garply")
+
+    def install(self, spec, prefix):
+        cmake(".", *std_cmake_args)
+        make()
+        make("install")
