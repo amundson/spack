2684624c6744f66396a36d8d696edf0614bdf41b
James Amundson <amundson@fnal.gov>
2016-01-21 11:08:25 -0600

    add 1.0.1

--- var/spack/packages/garply/package.py
+++ var/spack/packages/garply/package.py
@@ -5,6 +5,8 @@ class Garply(Package):
     homepage = "https://github.com/amundson/garply"
     url      = "http://compacc.fnal.gov/~amundson/garply-1.0.tar.gz"
 
+    version('1.0.1', git='git@github.com:amundson/garply.git',
+            tag='v1.0.1')
     version('1.0', '13a24801512e5b577afde076d2e908d3')
 
     depends_on("cmake@3.0:")
