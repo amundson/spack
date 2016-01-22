fa75601385746eaaefcb37f144d17ae010394da3
James Amundson <amundson@fnal.gov>
2016-01-21 21:49:52 -0600

    corge up to version 1.0.4; also master branch

--- var/spack/packages/corge/package.py
+++ var/spack/packages/corge/package.py
@@ -5,7 +5,16 @@ class Corge(Package):
     homepage = "https://github.com/amundson/corge"
     url      = "http://compacc.fnal.gov/~amundson/corge-1.0.tar.gz"
 
+    version('1.0.4', git='git@github.com:amundson/corge.git',
+            tag='v1.0.4')
+    version('1.0.3', git='git@github.com:amundson/corge.git',
+            tag='v1.0.3')
+    version('1.0.2', git='git@github.com:amundson/corge.git',
+            tag='v1.0.2')
+    version('1.0.1', git='git@github.com:amundson/corge.git',
+            tag='v1.0.1')
     version('1.0', 'c83ec110f677b67e0496260ccc7ceb81')
+    version('master', git='git@github.com:amundson/corge.git')
 
     depends_on("cmake@3.0:")
     depends_on("quux")
