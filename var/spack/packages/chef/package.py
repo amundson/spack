26c046e9d5f312040b8a29361803a78b974a6e20
James Amundson <amundson@fnal.gov>
2016-01-22 22:25:27 -0600

    start of a chef package definition

---
+++ var/spack/packages/chef/package.py
@@ -0,0 +1,20 @@
+from spack import *
+
+class Chef(Package):
+    """FIXME: put a proper description of your package here."""
+    homepage = "http://www.example.com"
+    url      = "http://compacc.fnal.gov/~amundson/chef-2016-01-06.tar.bz2"
+
+    version('2016-0', '1174747188063a965ea5e792f94038b3')
+
+    depends_on("boost")
+    depends_on("fftw+mpi")
+    depends_on("python")
+    depends_on("cmake")
+    depends_on("py-numpy")
+
+    def install(self, spec, prefix):
+        cmake('.', *std_cmake_args)
+
+        make()
+        make("install")
