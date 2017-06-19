62e16e499a460e33fd4286ff1b4a6d736641a6d2
James Amundson <amundson@fnal.gov>
2017-06-19 14:54:14 -0500

    metasyn packages

---
+++ var/spack/repos/builtin/packages/quux/package.py
@@ -0,0 +1,18 @@
+from spack import *
+
+class Quux(CMakePackage):
+    """Quux is a toy package"""
+    homepage = "https://github.com/amundson/quux"
+    url      = "http://compacc.fnal.gov/~amundson/quux-1.0.tar.gz"
+
+    version('2.0.0', git='git@github.com:amundson/quux.git',
+            tag='v2.0.0')
+    version('1.0.3', git='git@github.com:amundson/quux.git',
+            tag='v1.0.3')
+    version('1.0.2', git='git@github.com:amundson/quux.git',
+            tag='v1.0.2')
+    version('1.0', 'e64c0dfbfdf43c17cd6aeeef79058734')
+
+    depends_on("cmake@3.0:")
+    depends_on("garply")
+
