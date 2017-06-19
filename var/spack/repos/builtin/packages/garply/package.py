bd2af9b70ef5a09c4ad828cfe42046faaefd7e65
James Amundson <amundson@fnal.gov>
2017-06-19 15:43:15 -0500

    update garply to 2.0

--- var/spack/repos/builtin/packages/garply/package.py
+++ var/spack/repos/builtin/packages/garply/package.py
@@ -1,42 +1,3 @@
-##############################################################################
-# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
-# Produced at the Lawrence Livermore National Laboratory.
-#
-# This file is part of Spack.
-# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
-# LLNL-CODE-647188
-#
-# For details, see https://github.com/llnl/spack
-# Please also see the LICENSE file for our notice and the LGPL.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License (as
-# published by the Free Software Foundation) version 2.1, February 1999.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
-# conditions of the GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-##############################################################################
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install garply
-#
-# You can edit this file again by typing:
-#
-#     spack edit garply
-#
-# See the Spack documentation for more information on packaging.
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
 from spack import *
 
 
@@ -47,6 +8,8 @@ class Garply(CMakePackage):
     homepage = "http://www.example.com"
     url      = "http://compacc.fnal.gov/~amundson/garply-1.0.tar.gz"
 
+    version('2.0.0', git='git@github.com:amundson/garply.git',
+                        tag='v2.0.0')
     version('1.0', '13a24801512e5b577afde076d2e908d3')
 
     # FIXME: Add dependencies if required.
