1c8f691df5293b754e92993cd4ff6645cf45618c
James Amundson <amundson@fnal.gov>
2017-06-19 15:51:33 -0500

    update metasyn packages urls

--- var/spack/repos/builtin/packages/corge/package.py
+++ var/spack/repos/builtin/packages/corge/package.py
@@ -5,18 +5,18 @@ class Corge(CMakePackage):
     homepage = "https://github.com/amundson/corge"
     url      = "http://compacc.fnal.gov/~amundson/corge-1.0.tar.gz"
 
-    version('2.0.0', git='git@github.com:amundson/corge.git',
+    version('2.0.0', git='https://github.com/amundson/corge.git',
             tag='v2.0.0')
-    version('1.0.4', git='git@github.com:amundson/corge.git',
+    version('1.0.4', git='https://github.com/amundson/corge.git',
             tag='v1.0.4')
-    version('1.0.3', git='git@github.com:amundson/corge.git',
+    version('1.0.3', git='https://github.com/amundson/corge.git',
             tag='v1.0.3')
-    version('1.0.2', git='git@github.com:amundson/corge.git',
+    version('1.0.2', git='https://github.com/amundson/corge.git',
             tag='v1.0.2')
-    version('1.0.1', git='git@github.com:amundson/corge.git',
+    version('1.0.1', git='https://github.com/amundson/corge.git',
             tag='v1.0.1')
     version('1.0', 'c83ec110f677b67e0496260ccc7ceb81')
-    version('master', git='git@github.com:amundson/corge.git')
+    version('master', git='https://github.com/amundson/corge.git')
 
     depends_on("cmake@3.0:")
     depends_on("quux")
